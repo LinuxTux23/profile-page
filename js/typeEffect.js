6f4de225597b8d4b2d8a3b64be50e1677d9d71c0
Benedikt <benedikt2302@hormail.de>
2021-04-13 22:12:21 +0200

    Add Header Type Effect

---
+++ js/typeEffect.js
@@ -0,0 +1,16 @@
+// Script for Header Type Effect
+let text = 'CodingSheep';
+let velocity = 100;
+let i = 0;
+
+function typeWriter() {
+    let typeArea = document.getElementById('typeEffectArea');
+    let typeCursor = document.getElementById('typeEffectCursor');
+    typeCursor.hidden = false;
+
+    if(i < text.length){
+        typeArea.innerText += text.charAt(i);
+        i++;
+        setTimeout(typeWriter, velocity);
+    }
+}
